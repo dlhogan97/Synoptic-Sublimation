5884c134f7f864500a7dfc4c2188c48dad0015a4
Danny Hogan <dlhogan@uw.edu>
2024-01-11 15:17:57 -0800

    Updated plots in winter long exploration going

--- 02_analysis/data_exploration/scripts/helper_funcs.py
+++ 02_analysis/data_exploration/scripts/helper_funcs.py
@@ -1,6 +1,77 @@
 import pandas as pd
 import numpy as np
 
+WIND_VARIABLES = [
+# Sonic Anemometer Data for 4 towers
+'spd_1m_uw',     'dir_1m_uw',     'u_1m_uw',   'v_1m_uw',   'w_1m_uw',
+'spd_3m_uw',     'dir_3m_uw',     'u_3m_uw',   'v_3m_uw',   'w_3m_uw',
+'spd_10m_uw',    'dir_10m_uw',    'u_10m_uw',  'v_10m_uw',  'w_10m_uw',
+'spd_1m_ue',     'dir_1m_ue',     'u_1m_ue',   'v_1m_ue',   'w_1m_ue',
+'spd_3m_ue',     'dir_3m_ue',     'u_3m_ue',   'v_3m_ue',   'w_3m_ue',
+'spd_10m_ue',    'dir_10m_ue',    'u_10m_ue',  'v_10m_ue',  'w_10m_ue',
+'spd_1m_d',     'dir_1m_d',     'u_1m_d',   'v_1m_d',   'w_1m_d',
+'spd_3m_d',     'dir_3m_d',     'u_3m_d',   'v_3m_d',   'w_3m_d',
+'spd_10m_d',    'dir_10m_d',    'u_10m_d',  'v_10m_d',  'w_10m_d',
+'spd_2m_c',     'dir_2m_c',     'u_2m_c',   'v_2m_c',   'w_2m_c',
+'spd_3m_c',     'dir_3m_c',     'u_3m_c',   'v_3m_c',   'w_3m_c',
+'spd_5m_c',     'dir_5m_c',     'u_5m_c',   'v_5m_c',   'w_5m_c',
+'spd_10m_c',    'dir_10m_c',    'u_10m_c',  'v_10m_c',  'w_10m_c',
+'spd_15m_c',    'dir_15m_c',    'u_15m_c',  'v_15m_c',  'w_15m_c',
+'spd_20m_c',    'dir_20m_c',    'u_20m_c',  'v_20m_c',  'w_20m_c',
+]
+TURBULENCE_VARIABLES = [
+    'tc_1m_uw',        'u_u__1m_uw',    'v_v__1m_uw',    'w_w__1m_uw',    
+        'u_w__1m_uw',    'v_w__1m_uw',  'u_tc__1m_uw',  'v_tc__1m_uw',   'u_h2o__1m_uw',  'v_h2o__1m_uw',   'w_tc__1m_uw',   'w_h2o__1m_uw',
+    'tc_3m_uw',        'u_u__3m_uw',    'v_v__3m_uw',    'w_w__3m_uw',    
+        'u_w__3m_uw',    'v_w__3m_uw',  'u_tc__3m_uw',  'v_tc__3m_uw',   'u_h2o__3m_uw',  'v_h2o__3m_uw',   'w_tc__3m_uw',   'w_h2o__3m_uw',
+    'tc_10m_uw',      'u_u__10m_uw',   'v_v__10m_uw',   'w_w__10m_uw',   
+        'u_w__10m_uw',   'v_w__10m_uw', 'u_tc__10m_uw', 'v_tc__10m_uw',  'u_h2o__10m_uw', 'v_h2o__10m_uw',  'w_tc__10m_uw',  'w_h2o__10m_uw',
+
+    'tc_1m_ue',        'u_u__1m_ue',    'v_v__1m_ue',    'w_w__1m_ue',    
+        'u_w__1m_ue',    'v_w__1m_ue',  'u_tc__1m_ue',  'v_tc__1m_ue',   'u_h2o__1m_ue',  'v_h2o__1m_ue',   'w_tc__1m_ue',   'w_h2o__1m_ue',
+    'tc_3m_ue',        'u_u__3m_ue',    'v_v__3m_ue',    'w_w__3m_ue',    
+        'u_w__3m_ue',    'v_w__3m_ue',  'u_tc__3m_ue',  'v_tc__3m_ue',   'u_h2o__3m_ue',  'v_h2o__3m_ue',   'w_tc__3m_ue',   'w_h2o__3m_ue',
+    'tc_10m_ue',      'u_u__10m_ue',   'v_v__10m_ue',   'w_w__10m_ue',   
+        'u_w__10m_ue',   'v_w__10m_ue', 'u_tc__10m_ue', 'v_tc__10m_ue',  'u_h2o__10m_ue', 'v_h2o__10m_ue',  'w_tc__10m_ue',  'w_h2o__10m_ue',
+
+    'tc_1m_d',         'u_u__1m_d',    'v_v__1m_d',    'w_w__1m_d',    
+        'u_w__1m_d',    'v_w__1m_d',  'u_tc__1m_d',  'v_tc__1m_d',   'u_h2o__1m_d',  'v_h2o__1m_d',   'w_tc__1m_d',   'w_h2o__1m_d',
+    'tc_3m_d',         'u_u__3m_d',    'v_v__3m_d',    'w_w__3m_d',    
+        'u_w__3m_d',    'v_w__3m_d',  'u_tc__3m_d',  'v_tc__3m_d',   'u_h2o__3m_d',  'v_h2o__3m_d',   'w_tc__3m_d',   'w_h2o__3m_d',
+    'tc_10m_d',       'u_u__10m_d',   'v_v__10m_d',   'w_w__10m_d',   
+        'u_w__10m_d',   'v_w__10m_d', 'u_tc__10m_d', 'v_tc__10m_d',  'u_h2o__10m_d', 'v_h2o__10m_d',  'w_tc__10m_d',  'w_h2o__10m_d',
+
+    'tc_2m_c',     'u_u__2m_c',    'v_v__2m_c',    'w_w__2m_c',    
+        'u_w__2m_c',    'v_w__2m_c',  'u_tc__2m_c',  'v_tc__2m_c',   'u_h2o__2m_c',  'v_h2o__2m_c',   'w_tc__2m_c',   'w_h2o__2m_c',
+    'tc_3m_c',     'u_u__3m_c',    'v_v__3m_c',    'w_w__3m_c',    
+        'u_w__3m_c',    'v_w__3m_c',  'u_tc__3m_c',  'v_tc__3m_c',   'u_h2o__3m_c',  'v_h2o__3m_c',   'w_tc__3m_c',   'w_h2o__3m_c',
+    'tc_5m_c',     'u_u__5m_c',    'v_v__5m_c',    'w_w__5m_c',    
+        'u_w__5m_c',    'v_w__5m_c',  'u_tc__5m_c',  'v_tc__5m_c',   'u_h2o__5m_c',  'v_h2o__5m_c',   'w_tc__5m_c',   'w_h2o__5m_c',
+    'tc_10m_c',   'u_u__10m_c',   'v_v__10m_c',   'w_w__10m_c',   
+        'u_w__10m_c',   'v_w__10m_c', 'u_tc__10m_c', 'v_tc__10m_c',  'u_h2o__10m_c', 'v_h2o__10m_c',  'w_tc__10m_c',  'w_h2o__10m_c',
+    'tc_15m_c',   'u_u__15m_c',   'v_v__15m_c',   'w_w__15m_c',   
+        'u_w__15m_c',   'v_w__15m_c', 'u_tc__15m_c', 'v_tc__15m_c',  'u_h2o__15m_c', 'v_h2o__15m_c',  'w_tc__15m_c',  'w_h2o__15m_c',
+    'tc_20m_c',   'u_u__20m_c',   'v_v__20m_c',   'w_w__20m_c',   
+        'u_w__20m_c',   'v_w__20m_c', 'u_tc__20m_c', 'v_tc__20m_c',  'u_h2o__20m_c', 'v_h2o__20m_c',  'w_tc__20m_c',  'w_h2o__20m_c',
+]
+
+TEMPERATURE_VARIABLES = [    
+    # Temperature & Relative Humidity Array 
+    'T_2m_c', 'T_3m_c', 'T_4m_c', 'T_5m_c', 'T_6m_c', 'T_7m_c', 'T_8m_c', 'T_9m_c', 'T_10m_c',
+    'T_11m_c', 'T_12m_c', 'T_13m_c', 'T_14m_c', 'T_15m_c', 'T_16m_c', 'T_17m_c', 'T_18m_c', 'T_19m_c', 'T_20m_c',
+
+    'RH_2m_c', 'RH_3m_c', 'RH_4m_c', 'RH_5m_c', 'RH_6m_c', 'RH_7m_c', 'RH_8m_c', 'RH_9m_c', 'RH_10m_c',
+    'RH_11m_c','RH_12m_c','RH_13m_c','RH_14m_c','RH_15m_c','RH_16m_c','RH_17m_c','RH_18m_c','RH_19m_c','RH_20m_c'
+]
+PRESSURE_VARIABLES = [
+    # Pressure Sensors
+    'P_20m_c',
+    'P_10m_c', 'P_10m_d', 'P_10m_uw', 'P_10m_ue'
+]
+SNOW_FLUX = [
+    # Blowing snow/FlowCapt Sensors
+    'SF_avg_1m_ue', 'SF_avg_2m_ue',
+]
 # create a function to setup a dataframe for a windrose plot in plotly
 def create_windrose_df(df, wind_dir_var, wind_spd_var):
     """
